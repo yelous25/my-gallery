9a8186064edcbc328ba2c33c80b53764555f5a9f
Yelous25 <105781548+yelous25@users.noreply.github.com>
2023-08-22 18:38:08 +0000

    fix: asset handling urls

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 document.querySelectorAll('.gallery figure').forEach((figure, key) => {
+  let imgURL = new URL(`/assets/pictures/photo${key + 1}.png`, import.meta.url).href
   figure.classList.add('photo', `photo-${key + 1}`)
   figure.innerHTML = `
     <img
-      src="/assets/pictures/photo${key + 1}.png"
+      src="${imgURL}"
       alt="photo${key + 1}"
     />`
 })
